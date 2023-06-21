b12d7043fa3936b4e289ea355f1927da7e6db14b
BradySBaker <bradysamuelbaker@gmail.com>
2023-06-21 10:50:34 -0700

    Created temp data for a toy

--- client/src/components/IndividualToy/index.jsx
+++ client/src/components/IndividualToy/index.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import tempData from './tempData';
+
 import PhotoCarousel from './components/PhotoCarousel';
 import ToyInfo from './components/ToyInfo';
 import ToyReserve from './components/ToyReserve';
---
+++ client/src/components/IndividualToy/tempData.js
@@ -0,0 +1,16 @@
+export default {
+  name: 'LumiTEK R/C - Neon Giant Truck',
+  category: 'Remote Control',
+  rating: 4,
+  user: 'jacob',
+  description: '4WD. Awesome High speed RGB truck! Works great outdoors and drifts! My son loves this thing and its super fast, in awesome condition!',
+  originalPrice: 99.99,
+  rentalPrice: 20.87,
+  deliveryMethod: 'Pickup,Ship',
+  paymentMethod: 'Cash,Venmo',
+  photos: [
+    'https://i5.walmartimages.com/asr/0778c03b-fe37-4d52-a6b3-a11493f03e15.414ae51e69bdeecdeae08e395ed1f64d.jpeg?odnHeight=768&odnWidth=768&odnBg=FFFFFF',
+    'https://cdn.shopify.com/s/files/1/0505/5715/7566/products/b1value.jpg?v=1637174506',
+    'https://thetoyinsider.com/wp-content/uploads/2023/03/WeCoolToys_Lumitek-800x450.webp'
+  ]
+};
