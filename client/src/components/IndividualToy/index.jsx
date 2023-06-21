54155c152da9613538f75fe89e9e2eba98bd5e94
BradySBaker <bradysamuelbaker@gmail.com>
2023-06-21 12:44:19 -0700

    Added back and like button place holder with basic styling

--- client/src/components/IndividualToy/index.jsx
+++ client/src/components/IndividualToy/index.jsx
@@ -9,10 +9,17 @@ import ToyReserve from './components/ToyReserve.jsx';
 const IndividualToy = () => {
   const [toy, setToy] = useState(tempData);
   return (
-    <div>
+    <div className='bg-gray-800 text-white'>
       <div className='flex justify-center space-x-16'>
         <div>{toy.name}</div>
         <div>{toy.rating}</div>
+      </div>
+
+      <div className='z-10 flex justify-center relative space-x-60 translate-y-10'>
+        <div className='btn btn-sm text-xs btn-square bg-gray-900 text-white'>❮</div>
+        <button className="btn btn-sm btn-square bg-gray-900">
+          <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="red"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" /></svg>
+        </button>
       </div>
       <PhotoCarousel toy={toy}/>
       <ToyInfo/>
