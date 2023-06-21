d7db483d51066bfe0904506a18aa2a9730875cae
BradySBaker <bradysamuelbaker@gmail.com>
2023-06-20 21:29:14 -0700

    Added some example tailwind css in App.jsx to confirm working

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const App = () => {
-  return <div>Hello World Deployed!!!</div>;
+  return <div className='text-red-950'>Hello World Deployed!!!</div>;
 };
 
 export default App;
