30bc8c024753dd5c6e0492fdfdd970d72097f8f5
kyle <kyle.dick0811@gmail.com>
2023-06-20 21:10:27 -0600

    fixed App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,9 +2,7 @@ import React from 'react';
 import RenteeProfile from './components/RenteeProfile/renteeProfile.js';
 
 const App = () => {
-  return <div>
-    Hello World
-  </div>;
+  return <div>Hello World Deployed!!!</div>;
 };
 
 export default App;
