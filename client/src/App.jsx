629ab14927ceaabfbd840b7d395d118a442c6e93
BradySBaker <bradysamuelbaker@gmail.com>
2023-06-22 13:43:29 -0700

    Custom star style added star-icon-filled/unfilled using daisy ui svg

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 // import RenteeProfile from './components/RenteeProfile/renteeProfile.js';
+import IndividualToy from './components/IndividualToy/IndividualToy.jsx';
 
 const App = () => {
   return (
     <>
-      <div className='text-red-950'>Hello World Deployed!!!</div>
-      <button className='btn btn-outline btn-primary'>Daisy Button</button>
+      {/* <div className='text-red-950'>Hello World Deployed!!!</div>
+      <button className='btn btn-outline btn-primary'>Daisy Button</button> */}
+      <IndividualToy />
     </>
   );
 };
