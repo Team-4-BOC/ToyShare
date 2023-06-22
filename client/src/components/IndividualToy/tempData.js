f09119a19e26c4afd702e2761e100e7366c6a85c
BradySBaker <bradysamuelbaker@gmail.com>
2023-06-21 22:27:20 -0700

    Added a SharedComponents folder with StarWidget component

--- client/src/components/IndividualToy/tempData.js
+++ client/src/components/IndividualToy/tempData.js
@@ -11,7 +11,7 @@ export default {
   photos: [
     'https://i5.walmartimages.com/asr/0778c03b-fe37-4d52-a6b3-a11493f03e15.414ae51e69bdeecdeae08e395ed1f64d.jpeg?odnHeight=768&odnWidth=768&odnBg=FFFFFF',
     'https://cdn.shopify.com/s/files/1/0505/5715/7566/products/b1value.jpg?v=1637174506',
-    'https://thetoyinsider.com/wp-content/uploads/2023/03/WeCoolToys_Lumitek-800x450.webp'
+    'https://i5.walmartimages.com/asr/dc8d82bd-f819-4d17-8c92-2543afb1d78b.88cca436288ff489fd12f0ea861ccc5e.jpeg?odnHeight=768&odnWidth=768&odnBg=FFFFFF'
   ],
   location: 'Sanfrancisco, California',
   dates: ['2023-06-17', '2023-06-18', '2023-06-19', '2023-06-20', '2023-06-21', '2023-06-22', '2023-06-23', '2023-06-24', '2023-06-25', '2023-06-26', '2023-06-27', '2023-06-28', '2023-06-29']
