47a2f1df405db8d333328852e6513c9044b89b29
Christian Willsea <christianwillsea@gmail.com>
2023-06-21 23:07:03 -0700

    Update toys post and circle ci congi

--- server/models/toys.js
+++ server/models/toys.js
@@ -12,7 +12,6 @@ module.exports = {
   // },
   post: (data) => {
     const values = [data.toy_name, data.category_id, data.rating, data.user_id, data.toy_description, data.original_price, data.rental_price, data.delivery_method, data.payment_method];
-    // return db.query('INSERT INTO toyshare.toys(toy_name, category_id, rating, user_id, toy_description, original_price, rental_price, delivery_method, payment_method) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9);', values);
-    return db.query('SELECT * FROM toyshare.toys;');
+    return db.query('INSERT INTO toyshare.toys(toy_name, category_id, rating, user_id, toy_description, original_price, rental_price, delivery_method, payment_method) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9);', values);
   }
 };
