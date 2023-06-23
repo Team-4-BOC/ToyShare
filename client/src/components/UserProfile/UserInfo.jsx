ca971f8400420f2f8aedc19bf714a0343db8e2a4
Justin Cheng <jucheng.ca@gmail.com>
2023-06-22 23:47:55 -0700

    add inline style for user profile add edit profile & add/edit toys buttons

--- client/src/components/UserProfile/UserInfo.jsx
+++ client/src/components/UserProfile/UserInfo.jsx
@@ -1,16 +1,20 @@
 import React, { useState, useEffect } from 'react';
+import StarCreator from '../SharedComponents/StarCreator.js';
 
 function UserInfo ({ userData }) {
-  console.log(userData)
   return (
-    <div className="card card-compact w-80 bg-base-80 shadow-xl">
+  <div className="card card-compact w-80 bg-base-80 shadow-xl">
     <figure><img src={userData.photo} /></figure>
     <div className="card-body">
-      <h2 className="user-name">{userData.user.first_name} {userData.user.last_name}</h2>
-      <h2 className="user-location">{userData.user.city_state}</h2>
-      <p>Proud parent of amazing kids. Check out my inventory of toys for rental!</p>
+      <h2 className="user-name" style={{ fontSize: 25, fontWeight: 'bold', display: 'flex', flexDirection: 'column', alignItems: 'center' }}>{userData.user.first_name} {userData.user.last_name}</h2>
+      <h2 className="user-location" style={{ fontSize: 15, display: 'flex', flexDirection: 'column', alignItems: 'center' }}>{userData.user.city_state}</h2>
+        <div className="user-rating" style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+          {StarCreator(5)}
+        </div>
+      <p style={{ fontSize: 15, display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>Proud parent of amazing kids. Check out my inventory of toys for rental!</p>
       <div className="card-actions justify-end">
       </div>
+      <button className="btn-sm" style={{ fontSize: 12, textAlign: 'right' }}>Edit Profile</button>
     </div>
   </div>
   );
--- client/src/components/UserProfile/UserToys.jsx
+++ client/src/components/UserProfile/UserToys.jsx
@@ -1,23 +1,28 @@
 import React, { useState, useEffect } from 'react';
 
 function UserToys ({ userData }) {
+  const titleStyle = {
+    fontSize: 20,
+    fontWeight: 'bold'
+  };
   return (
     <div className="user-toys">
       {/* //////// RENTAL INVENTORY LIST //////// */}
       <ul className="menu bg-base-200 w-100 rounded-box">
       <li>
-        <h2 className="rental-inventory">Rental Inventory</h2>
+        <h2 className="rental-inventory" style={titleStyle}>Rental Inventory</h2>
         <ul>
         {userData.inventory.map(toy => <div key={toy.id}>• {toy.toy_name}</div>)}
         </ul>
       </li>
+      <button className="btn-sm" style={{ fontSize: 12, textAlign: 'right' }}>Add/Edit Toys</button>
     </ul>
     <br></br>
     {/* //////// CURRENTLY RENTING LIST -- excluding for now //////// */}
     {/* //////// RENTAL HISTORY LIST //////// */}
     <ul className="menu bg-base-200 w-100 rounded-box">
       <li>
-        <h2 className="rental-history">Rental History</h2>
+        <h2 className="rental-history" style={titleStyle}>Rental History</h2>
         <ul>
         {userData.history.map(toy => <div key={toy.id}>• {toy.toy_name}</div>)}
         </ul>
@@ -27,7 +32,7 @@ function UserToys ({ userData }) {
     {/* //////// SAVED TOYS LIST //////// */}
     <ul className="menu bg-base-200 w-100 rounded-box">
       <li>
-        <h2 className="saved">Saved Toys</h2>
+        <h2 className="saved" style={titleStyle}>Saved Toys</h2>
         <ul>
         {userData.saved.map(toy => <div key={toy.id}>• {toy.toy_name}</div>)}
         </ul>
