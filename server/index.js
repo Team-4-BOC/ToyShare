f5a6985ec41a066ee1c191d90a68ae3b2dd2928b
fmbandit <itshardtosaynotknowing@gmail.com>
2023-06-23 02:40:43 -0400

    added json middleware

--- server/index.js
+++ server/index.js
@@ -13,7 +13,8 @@ app.use(
   })
 );
 
-app.use(express.static(path.join(__dirname, '../client/dist')));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, '../client/dist')))
 
 app.use('/', routes);
 
