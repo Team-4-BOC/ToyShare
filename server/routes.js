39798eb7a3037ca128d66b6a79108c5ab9c09f43
fmbandit <itshardtosaynotknowing@gmail.com>
2023-06-23 01:25:21 -0400

    controller/model exportable and available for bookings

--- server/routes.js
+++ server/routes.js
@@ -10,6 +10,11 @@ router.post('/toys', controllers.toys.post);
 
 router.get('/user', controllers.users.getOne);
 
+router.post('/bookings/getAllBooked', controllers.bookings.getAllBooked);
+
+router.post('/bookings/getAllAvailable', controllers.bookings.getAllAvailable);
+
+
 // Examples:
 
 // router.get('/products/:product_id', controllers.product.getOne);
