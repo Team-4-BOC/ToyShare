cae4ad53070e6671bf297590c6b07eb1c4c44f2e
BradySBaker <bradysamuelbaker@gmail.com>
2023-06-23 23:12:37 -0700

    Photo counter working, fixed button positions/styles

--- client/src/components/IndividualToy/IndividualToy.jsx
+++ client/src/components/IndividualToy/IndividualToy.jsx
@@ -53,17 +53,11 @@ const IndividualToy = ({ testing, setPage, toyId, userId }) => {
 
   return (
     <div className='absolute overflow-y-scroll min-h-screen'>
-      <div className='flex justify-center space-x-5 bg-gray-900 '>
+      <div className='flex justify-center space-x-5 bg-gray-900'>
       <div className='text-lg font-bold text-white' data-testid='it-toy-name'>{toy.name}</div>
         {StarCreator(toy.rating)}
       </div>
-      <div className='z-10 flex justify-center relative space-x-60 translate-y-10'>
-        <div className='btn btn-sm text-xs btn-square bg-gray-900 text-white'>❮</div>
-        <button className="btn btn-sm btn-square bg-gray-900" onClick={handleSave}>
-          <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4" fill={toy.saved ? 'red' : 'none'} viewBox="0 0 24 24" stroke="red"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" /></svg>
-        </button>
-      </div>
-      <PhotoCarousel toy={toy}/>
+      <PhotoCarousel toy={toy} handleSave={handleSave}/>
       <ToyInfo toy={toy} setPage={setPage}/>
       <ToyReserve toy={toy} setPage={setPage}/>
     </div>
--- client/src/components/IndividualToy/components/ToyReserve.jsx
+++ client/src/components/IndividualToy/components/ToyReserve.jsx
@@ -4,12 +4,12 @@ const ToyReserve = ({ toy, setPage }) => {
   const nextDate = new Date(toy.next_date).toLocaleString();
   return (
     <div className='bottom-0 fixed'>
-      <div className='flex w-full px-4 py-5 bg-gray-950 bottom-0 fixed border-2 border-white'>
+      <div className='flex w-full px-4 py-5 bg-gray-950 bottom-0 fixed border-2 border-b-stone-600'>
         <div data-testid='it-originPrice' className='mr-5 font-bold line-through text-red-700'>${toy.original_price}</div>
         <div data-testid='it-rentalPrice' className='font-bold text-green-100'>${toy.rental_price}/month</div>
-        <div data-testid='it-reserveButton' className='btn btn-primary ml-auto bg-red-600 text-white' onClick={() => setPage(6)}>Reserve</div>
+        <div data-testid='it-reserveButton' className='rounded-lg btn btn-primary ml-auto bg-red-600 text-white' onClick={() => setPage(6)}>Reserve</div>
       </div>
-      <div data-testid='it-next_date' className='font-extralight -translate-y-3.5 translate-x-4'>Next Available: {nextDate.split(',')[0]}</div>
+      <div data-testid='it-next_date' className='font-extralight -translate-y-3.5 translate-x-4 text-white'>Next Available: {nextDate.split(',')[0]}</div>
     </div>
   );
 };
