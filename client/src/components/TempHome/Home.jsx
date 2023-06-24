1a05d8eba324741e28db47f379bcf97f17f96cd4
BradySBaker <bradysamuelbaker@gmail.com>
2023-06-23 20:52:05 -0700

    Added random toy display to home page

--- client/src/components/TempHome/Home.jsx
+++ client/src/components/TempHome/Home.jsx
@@ -7,8 +7,9 @@ function Home ({ setToyId, setToyUserId, setPage }) {
   const [toys, setToys] = useState([]);
 
   useEffect(() => {
-    const count = 30;
-    const page = 1;
+    const count = 10;
+    const page = Math.floor((Math.random() * 3)) + 1;
+    console.log(page);
     axios.get('/toys', ({ params: { page, count } }))
       .then((response) => {
         setToys(response.data);
