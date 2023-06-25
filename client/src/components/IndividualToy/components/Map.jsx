3a31a68c9b63b095e6e6fb64b6a0a60eedc90062
BradySBaker <bradysamuelbaker@gmail.com>
2023-06-25 12:48:06 -0700

    Worked on shrinking icon

--- client/src/components/IndividualToy/components/Map.jsx
+++ client/src/components/IndividualToy/components/Map.jsx
@@ -2,11 +2,11 @@ import React, { useState, useEffect } from 'react';
 
 import axios from 'axios';
 
-import { GoogleMap, useLoadScript, Marker } from '@react-google-maps/api';
+import { GoogleMap, useLoadScript, Marker} from '@react-google-maps/api';
 
 import googleMapsApiKey from './googleMapsApiKey.js'; // Restricted key!
 
-const Map = ({ city, state }) => {
+const Map = ({ city, state, iconImage }) => {
   const { isLoaded } = useLoadScript({ googleMapsApiKey });
 
   const [coordinates, setCoordinates] = useState(false);
@@ -26,7 +26,7 @@ const Map = ({ city, state }) => {
       <div className='absolute w-full h-full flex justify-center z-20'>
         {isLoaded && coordinates
           ? <GoogleMap zoom={5} center={{ lat: coordinates.lat, lng: coordinates.lng }} mapContainerClassName='w-96 h-96 -translate-y-60' >
-              <Marker position={{ lat: coordinates.lat, lng: coordinates.lng }}/>
+              <Marker position={{ lat: coordinates.lat, lng: coordinates.lng }} icon={{ url: iconImage, scaledSize: new window.google.maps.Size(20, 20) }} />
           </GoogleMap>
           : <div>Loading Google Maps...</div>
         }
