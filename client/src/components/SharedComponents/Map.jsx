375d08db6e7f1c649e46245e2dae6ac569cbaa2e
BradySBaker <bradysamuelbaker@gmail.com>
2023-06-26 12:31:27 -0700

    Map component functional with both single and multiple toys

--- client/src/components/SharedComponents/Map.jsx
+++ client/src/components/SharedComponents/Map.jsx
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import MapBox, { Marker } from 'react-map-gl';
 
 import mapBoxKey from './mapBoxKey';
 
 const Map = ({ latLng, iconImage, toys }) => {
-  const coordinates = latLng.split(',');
+  const [coordinates, setCoordinates] = useState();
+  const [zoom, setZoom] = useState();
+  console.log(coordinates);
+  const setMap = () => {
+    if (toys !== undefined) {
+      setCoordinates([37.05247, 95.42464]);
+      setZoom(1);
+    } else {
+      setCoordinates(latLng.split(','));
+      setZoom(5);
+    }
+  };
+  useEffect(setMap, []);
 
   const handleViewStateChange = (props) => {
     const markerElements = document.querySelectorAll('.marker-element');
@@ -18,20 +30,32 @@ const Map = ({ latLng, iconImage, toys }) => {
 
   return (
       <div className='absolute w-full h-full flex justify-center z-20'>
-        <MapBox initialViewState={{ latitude: coordinates[0], longitude: coordinates[1], zoom: 5 }} style={{ width: window.innerWidth + 'px', height: window.innerHeight / 2 + 'px', transform: 'translateY(-80vw)' }} mapboxAccessToken={mapBoxKey} mapStyle="mapbox://styles/mapbox/streets-v9" onZoom={handleViewStateChange}>
-          <Marker latitude={coordinates[0]} longitude={coordinates[1]} anchor="bottom" >
+        {coordinates !== undefined
+          ? <MapBox initialViewState={{ latitude: coordinates[0], longitude: coordinates[1], zoom }} style={{ width: window.innerWidth + 'px', height: window.innerHeight / 2 + 'px', transform: 'translateY(-80vw)' }} mapboxAccessToken={mapBoxKey} mapStyle="mapbox://styles/mapbox/streets-v9" onZoom={handleViewStateChange}>
             {toys !== undefined
               ? toys.map((toy, idx) => {
-                return (<div key={idx * 10}>Hello world</div>);
+                const markerCoordinates = toy.latLng.split(',');
+                return (
+                  <Marker latitude={markerCoordinates[0]} longitude={markerCoordinates[1]} anchor="bottom" key={idx * 10}>
+                    <div className="icon-container">
+                      <div className="icon marker-element" style={{ width: '45px', height: '45px' }}>
+                        <div className="marker-icon" />
+                        <img src={toy.photos[0]} alt="Image" />
+                      </div>
+                    </div>
+                  </Marker>
+                );
               })
-              : <div className="icon-container">
-                <div className="icon marker-element" style={{ width: '45px', height: '45px' }}>
-                  <div className="marker-icon" />
-                  <img src={iconImage} alt="Image" />
+              : <Marker latitude={coordinates[0]} longitude={coordinates[1]} anchor="bottom" >
+                  <div className="icon-container">
+                  <div className="icon marker-element" style={{ width: '45px', height: '45px' }}>
+                    <div className="marker-icon" />
+                    <img src={iconImage} alt="Image" />
+                  </div>
                 </div>
-              </div>}
-        </Marker>
+              </Marker>}
         </MapBox>
+          : null}
       </div>
   );
 };
