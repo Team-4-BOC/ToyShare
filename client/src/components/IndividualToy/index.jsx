c6539aa2d0675b65941f72f8fb85e2ed4b0580e2
BradySBaker <bradysamuelbaker@gmail.com>
2023-06-26 18:13:08 -0700

    Removed index.jsx

--- client/src/components/IndividualToy/index.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import IndividualToy from './IndividualToy.jsx';
-
-const domNode = document.getElementById('root');
-const root = createRoot(domNode);
-root.render(<IndividualToy/>);
