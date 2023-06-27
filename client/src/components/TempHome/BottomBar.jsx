f9eeab7a8d2c08d9ea3601b3fab61d2d5e99ad17
BradySBaker <bradysamuelbaker@gmail.com>
2023-06-26 17:54:21 -0700

    Added mapBoxKey.js

--- client/src/components/TempHome/BottomBar.jsx
+++ client/src/components/TempHome/BottomBar.jsx
@@ -18,12 +18,12 @@ const BottomBar = ({ setPage, toys }) => {
 
   return (
     <>
-      {map ? <div className='w-screen h-screen bg-slate-400/40 top-0 left-0 fixed z-30' onClick={() => {setMap(false); console.log('occured')}}></div> : null}
+      {map ? <div className='w-screen h-screen bg-slate-400/40 top-0 left-0 fixed z-30' onClick={() => { setMap(false); }}></div> : null}
       <div className='fixed bottom-0 bg-white z-10 w-full shadow-md shadow-black rounded-tr-2xl rounded-tl-2xl flex justify-center space-x-14'>
         <button className="active:bg-gray-100 text-black py-3 px-4 rounded-full border border-solid" onClick={onAddToyClick}>
           AddToy
         </button>
-        {!map && toys !== undefined ? <img data-testid='it-map' className='inline-block hover:opacity-30 w-9 h-9 translate-y-2' src='./icons/mapIcon.png' onClick={() => setMap(true)}/> : null}
+        {!map && toys !== undefined ? <div className='shadow-sm shadow-black rounded-full'><img data-testid='it-map' className='inline-block hover:opacity-30 w-9 h-9 translate-y-2' src='./icons/mapIcon.png' onClick={() => setMap(true)}/></div> : null}
       </div>
       {map ? <Map toys={toys}/> : null}
     </>
