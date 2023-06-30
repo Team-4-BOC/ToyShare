4c91e5d63e7e16382074e9d6e69c5bc1d7fe424f
Kevin <kseng445@vt.edu>
2023-06-29 21:07:27 -0400

    Updated toycard layout, added some sort and filter functionalities

--- client/src/components/TempHome/BottomBar.jsx
+++ client/src/components/TempHome/BottomBar.jsx
@@ -6,7 +6,7 @@ import Map from '../SharedComponents/Map.jsx';
 
 import axios from 'axios';
 
-const BottomBar = ({ setPage, toysIDCoordsPhoto, setToysIDCoordsPhoto }) => {
+const BottomBar = ({ setPage, toysIDCoordsPhoto, setToysIDCoordsPhoto, userCoords, sort, setSort, filter, setFilter }) => {
   const [map, setMap] = useState(false);
 
   const fetchToysIDCoordsPhoto = () => {
@@ -34,10 +34,21 @@ const BottomBar = ({ setPage, toysIDCoordsPhoto, setToysIDCoordsPhoto }) => {
   return (
     <>
       {map ? <div className='w-screen h-screen bg-slate-400/40 top-0 left-0 fixed z-30' onClick={() => { setMap(false); }}></div> : null}
-      <div className='fixed bottom-0 bg-white z-10 w-full shadow-md shadow-black rounded-tr-2xl rounded-tl-2xl flex justify-center space-x-14'>
+      <div className='fixed bottom-0 bg-white z-10 w-full shadow-md shadow-black rounded-tr-2xl rounded-tl-2xl flex justify-center space-x-1'>
         <button className="active:bg-gray-100 text-black py-3 px-4 rounded-full border border-solid" onClick={onAddToyClick}>
           AddToy
         </button>
+        <select class="select sort" value={sort} onChange={() => { setSort(event.target.value) }}>
+          <option value="">Sort by</option>
+          <option value="newest">Newest</option>
+          <option value="rating">Rating</option>
+          <option value="distance">Distance</option>
+        </select>
+        <select class="select filter" value={filter} onChange={() => { setFilter(event.target.value) }}>
+          <option value="">Filter</option>
+          <option value="recommend">Recommend</option>
+          <option value="">All</option>
+        </select>
         {!map && toysIDCoordsPhoto !== undefined ? <div className='shadow-sm shadow-black rounded-full'><img data-testid='it-map' className='inline-block hover:opacity-30 w-9 h-9 translate-y-2' src='./icons/mapIcon.png' onClick={() => setMap(true)}/></div> : null}
       </div>
       {map ? <Map toysIDCoordsPhoto={toysIDCoordsPhoto}/> : null}
--- client/src/components/TempHome/Home.jsx
+++ client/src/components/TempHome/Home.jsx
@@ -2,21 +2,29 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import ToyCard from './ToyCard.jsx';
 
-function Home ({ setToyId, setToyUserId, setPage, searchTerm, userCoords, toysIDCoordsPhoto }) {
+function Home ({ setToyId, setToyUserId, setPage, searchTerm, sort, filter, userCoords, toysIDCoordsPhoto }) {
   const [toys, setToys] = useState([]);
   const [renderedToys, setRenderedToys] = useState([]);
 
   useEffect(() => {
-    getToys();
+    fetchToys();
   }, []);
 
   useEffect(() => {
     handleSearchTermChange();
   }, [searchTerm]);
 
-  const getToys = () => {
-    const count = 10;
-    const page = Math.floor(Math.random() * 2) + 1;
+  useEffect(() => {
+    handleSortChange();
+  }, [sort]);
+
+  useEffect(() => {
+    handleFilterChange();
+  }, [filter]);
+
+  const fetchToys = () => {
+    const count = 100;
+    const page = 1;
     axios
       .get('/toys', { params: { page, count } })
       .then((response) => {
@@ -46,22 +54,59 @@ function Home ({ setToyId, setToyUserId, setPage, searchTerm, userCoords, toysID
     }
   }
 
+  const handleSortChange = () => {
+    if (toys.length !== 0) {
+      if (sort == "") {
+        setRenderedToys(toys);
+      } else if (sort == "newest") {
+        setRenderedToys(toys); //need to change
+      } else if (sort == "rating") {
+        var sorted = JSON.parse(JSON.stringify(toys));
+        console.log(sorted)
+        sorted.sort((a, b) => {return b.rating - a.rating});
+        console.log(sorted)
+        setRenderedToys(sorted);
+      } else if (sort == "distance") {
+        setRenderedToys(toys); //need to change
+      }
+    } else {
+      setRenderedToys(toys);
+    }
+  }
+
+  const handleFilterChange = () => {
+    if (toys.length !== 0) {
+      if (filter == "") {
+        setRenderedToys(toys);
+      } else if (filter = "recommend") {
+        var mockUserRecommendCategories = [4, 7, 11]; // based on user's saved toys, probably max of 3 categories even if they have more different categories saved
+        var filtered = [];
+        for (let i = 0; i < toys.length; i++) {
+          if (mockUserRecommendCategories.includes(toys[i].category_id)) {
+            filtered.push(toys[i]);
+          }
+        }
+        setRenderedToys(filtered);
+      }
+    } else {
+      setRenderedToys(toys);
+    }
+  }
+
   const handleToyClick = (toyId, userId) => {
     setToyId(toyId);
     setToyUserId(userId);
     setPage(1);
   };
 
+  // console.log(renderedToys);
+
   return (
-    <div>
-      <ul>
-        {renderedToys.map((toy) => (
-          <div key={toy.id}>
-            <ToyCard toy={toy} handleToyClick={handleToyClick} userCoords={userCoords} toysIDCoordsPhoto={toysIDCoordsPhoto} />
-          </div>
-        ))}
-      </ul>
-    </div>
+    <ul className="grid grid-cols-2">
+      {renderedToys.map((toy) => (
+        <ToyCard toy={toy} handleToyClick={handleToyClick} userCoords={userCoords} toysIDCoordsPhoto={toysIDCoordsPhoto} />
+      ))}
+    </ul>
   );
 }
 
