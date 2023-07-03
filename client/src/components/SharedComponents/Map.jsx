f66e9e9ded56c61a43bd1ac1be767d049a1d84aa
BradySBaker <bradysamuelbaker@gmail.com>
2023-07-03 13:32:02 -0700

    Bugs fixed, scaling fixed

--- client/src/components/SharedComponents/Map.jsx
+++ client/src/components/SharedComponents/Map.jsx
@@ -1,6 +1,6 @@
-// import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
-// import MapBox, { Marker } from 'react-map-gl';
+import MapBox, { Marker } from 'react-map-gl';
 
 import mapBoxKey from './tempMapBoxKey.js';
 
@@ -15,6 +15,8 @@ const Map = ({ latLng, iconImage, toysIDCoordsPhoto, userCoords }) => {
     } else if (latLng !== undefined) {
       setCoordinates(latLng.split(','));
       setZoom(6);
+    } else {
+      setCoordinates([38.500000, -98.0000]);
     }
   };
   useEffect(setMap, [userCoords]);
@@ -22,6 +24,7 @@ const Map = ({ latLng, iconImage, toysIDCoordsPhoto, userCoords }) => {
   const handleViewStateChange = (props) => {
     const markerElements = document.querySelectorAll('.marker-element');
     const size = props.viewState.zoom * 20;
+    console.log(size);
     markerElements.forEach((element) => {
       element.style.height = size + 'px';
       element.style.width = size + 'px';
@@ -34,7 +37,10 @@ const Map = ({ latLng, iconImage, toysIDCoordsPhoto, userCoords }) => {
           ? <MapBox initialViewState={{ latitude: coordinates[0], longitude: coordinates[1], zoom }} style={{ width: window.innerWidth / 1.1 + 'px', height: window.innerHeight / 1.4 + 'px', position: 'fixed', top: '50%', right: '50%', transform: 'translate(50%, -50%)', zIndex: 30 }} mapboxAccessToken={mapBoxKey} mapStyle="mapbox://styles/mapbox/streets-v9" onZoom={handleViewStateChange}>
             {toysIDCoordsPhoto !== undefined
               ? toysIDCoordsPhoto.map((toy, idx) => {
-                if (curCoords[toy.latLng]) {
+                if (!toy.latlng) {
+                  return (null);
+                }
+                if (curCoords[toy.latlng]) {
                   // Fix me ------------
                 }
                 curCoords[toy.latLng] = true;
@@ -42,7 +48,7 @@ const Map = ({ latLng, iconImage, toysIDCoordsPhoto, userCoords }) => {
                 return (
                   <Marker latitude={markerCoordinates[0]} longitude={markerCoordinates[1]} anchor="bottom" key={idx * 10}>
                     <div className="icon-container">
-                      <div className="icon marker-element" style={{ width: '45px', height: '45px' }}>
+                      <div className="icon marker-element" style={{ width: '10px', height: '10px' }}>
                         <div className="marker-icon" />
                         <img src={toy.photo} alt="Image" />
                       </div>
@@ -52,7 +58,7 @@ const Map = ({ latLng, iconImage, toysIDCoordsPhoto, userCoords }) => {
               })
               : <Marker latitude={coordinates[0]} longitude={coordinates[1]} anchor="bottom" >
                   <div className="icon-container">
-                  <div className="icon marker-element" style={{ width: '45px', height: '45px' }}>
+                  <div className="icon marker-element" style={{ width: '100px', height: '100px' }}>
                     <div className="marker-icon" />
                     <img src={iconImage} alt="Image" />
                   </div>
@@ -64,4 +70,4 @@ const Map = ({ latLng, iconImage, toysIDCoordsPhoto, userCoords }) => {
   );
 };
 
-// export default Map;
+export default Map;
