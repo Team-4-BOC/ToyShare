11071a38d0d192a2e7a5ddee0a405ac6eb45967d
fmbandit <itshardtosaynotknowing@gmail.com>
2023-07-07 01:24:26 -0700

    working MVP calendar component

--- client/src/components/Checkout/bookingConfirmation.js
+++ client/src/components/Checkout/bookingConfirmation.js
@@ -1,76 +1,44 @@
 import React, { useState, useEffect } from "react";
+import axios from 'axios';
 
-/**
- * todo: 
- *  - query component to get the user's data
- *  - if not queryable, is there a token in local storage ? 
- *      (client needs a reference to who's logged in)
- *  
- */
+function BookingConfirmation({ toyId, userId, toyUserId, selectedDates }) {
+  const [toyData, setToyData] = useState(null);
+  const [toyUserData, setToyUserData] = useState(null);
 
-function getDummyData() {
-    const user = {
-        first_name: 'Nick',
-        last_name: 'Mann'
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const toyResponse = await axios.get(`/toy?toyId=${toyId}`);
+        setToyData(toyResponse.data);
+
+        const toyUserResponse = await axios.get(`/user?userId=${toyUserId}`);
+        setToyUserData(toyUserResponse.data);
+      } catch (err) {
+        console.error('Error fetching data: ', err);
+      }
     };
-    const toy = {
-        toy_photos: [
-            {
-                url: 'https://place-puppy.com/200x200'
-            },
-            {
-                url: 'https://place-puppy.com/201x201'
-            }
-        ],
-        toy_name: 'a puppy',
-        rental_price: 20
-    }
-    const dates_booked = [
-        'Thursday, June 22', 
-        'Friday, June 23'
-    ]
-    const checkoutData = {
-        user, 
-        toy,
-        dates_booked
-    }
-    return checkoutData;
-}
+    fetchData();
+  }, [toyId, toyUserId]);
 
-function BookingConfirmation() {
-    const [checkoutData, setCheckoutData] = useState(null);
+  if (!toyData || !toyUserData) {
+    return <div>Loading...</div>;
+  }
 
-    useEffect(() => {
-        // todo: replace with API call 
-        const dummyData = getDummyData();
-        setCheckoutData(dummyData);
-    }, []);
-
-    if (checkoutData === null) {
-        return <div>Loading...</div>;
-    }
-
-    return (
-        <div>
-            <h2>Booking Confirmation</h2>
-
-            <h3>User Info</h3>
-            <p>Name: {checkoutData.user.first_name} {checkoutData.user.last_name}</p>
-
-            <h3>Toy Info</h3>
-            <img src={checkoutData.toy.toy_photos[0].url} alt={checkoutData.toy.toy_name} />
-            <p>Name: {checkoutData.toy.toy_name}</p>
-            <p>Description: {checkoutData.toy.toy_description}</p>
-            <p>Rental Price: ${checkoutData.toy.rental_price} per day</p>
-
-            <h3>Booking Info</h3>
-            <p>Booked Dates:</p>
-            <ul>
-                {checkoutData.dates_booked.map((date, index) => <li key={index}>{date}</li>)}
-            </ul>
-            <p>Total Price: ${checkoutData.toy.rental_price * checkoutData.dates_booked.length}</p>
-        </div>
-    );
-}
+  return (
+    <div>
+      <h2>Booking Confirmation</h2>
+      <p>Toy Name: {toyData.name}</p>
+      <img src={toyData.image} alt={toyData.name} />
+      <p>Owner of Toy: {toyUserData.name} (e-mail {toyUserData.email} to coordinate payment + pickup)</p>
+      <p>Rental Price: ${toyData.rental_price} per day x {selectedDates.length} days = 
+      <b>${toyData.rental_price * selectedDates.length}</b> </p>
+      <p>Booked Dates:</p>
+      <ul>
+        {selectedDates.map((date, index) => <li key={index}>{date.toISOString().split('T')[0]}</li>)}
+      </ul>
+      <p>Total Price: ${toyData.rental_price * selectedDates.length}</p>
+    </div>
+  );
+};
 
 export default BookingConfirmation;
